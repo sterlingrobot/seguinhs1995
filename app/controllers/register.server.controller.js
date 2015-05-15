1dc452849688180cfbfcaca826e2c831a96b204b
Tor Gilbertson <sterlingrobot@gmail.com>
2015-05-14 20:51:26 -0600

    Add Update Registration

--- app/controllers/register.server.controller.js
+++ app/controllers/register.server.controller.js
@@ -12,7 +12,7 @@ var _ = require('lodash'),
 
 
 exports.charge = function(req, res, next) {
-	console.log(req);
+
 	var user = req.user,
 		meta = _.extend(user.address, {phone: user.phone}),
 		chg = {
@@ -28,28 +28,31 @@ exports.charge = function(req, res, next) {
 		  }
 		};
 
-	stripe.customers.create({
-		description: user.displayName,
-		email: user.email,
-		source: req.body.token,
-		metadata: meta
-	}, function(err, customer) {
+	if(user.registration && typeof user.registration.customer !== 'undefined') {
 
-		if (err) {
-			return res.status(400).send(err);
-		} else {
+		// User already has a Stripe customer id, assign it to this charge
+		chg = _.extend(chg, { customer: user.registration.customer });
+		processCharge();
 
-			chg = _.extend(chg, { customer: customer.id });
-			stripe.charges.create(chg, function(err, charge) {
+	} else {
+
+		stripe.customers.create({
+			description: user.displayName,
+			email: user.email,
+			source: req.body.token,
+			metadata: meta
+		}, function(err, customer) {
 
 				if (err) {
 					return res.status(400).send(err);
 				} else {
+					// Update user data with address info if provided
 					User.findOne({
 						_id: req.user._id
 					}, function(err, user) {
 						if(user) {
-							user.registration = charge;
+							user.address = user.address || {};
+							user.phone = user.phone || '';
 							user.save(function(err) {
 								if (err) {
 									return res.status(400).send({
@@ -65,12 +68,50 @@ exports.charge = function(req, res, next) {
 							});
 						}
 					});
+
+					chg = _.extend(chg, { customer: customer.id });
+					processCharge();
+
 				}
+			}
+		);
+	}
 
-			});
-		}
-	});
+	function processCharge() {
 
+		stripe.charges.create(chg, function(err, charge) {
+
+			if (err) {
+				return res.status(400).send(err);
+			} else {
+				User.findOne({
+					_id: req.user._id
+				}, function(err, user) {
+					if(user) {
+						user.registration = user.registration || {};
+						user.registration.registered = true;
+						user.registration.guests = charge.metadata.guests;
+						user.registration.payments = user.registration.payments || [];
+						user.registration.payments.push(charge);
+						user.save(function(err) {
+							if (err) {
+								return res.status(400).send({
+									message: errorHandler.getErrorMessage(err)
+								});
+							} else {
+								return res.jsonp(user);
+							}
+						});
+					} else {
+						res.status(400).send({
+							message: 'User id did not match any existing user'
+						});
+					}
+				});
+			}
+
+		});
+	}
 
 
 };
--- public/modules/core/controllers/register.client.controller.js
+++ public/modules/core/controllers/register.client.controller.js
@@ -1,13 +1,20 @@
 'use strict';
 
 
-angular.module('core').controller('RegisterController', ['$scope', '$window', '$log', '$timeout', 'Authentication', 'Payments',
-	function($scope, $window, $log, $timeout, Authentication, Payments) {
+angular.module('core').controller('RegisterController', ['$scope', '$window', '$log', '$timeout', 'Users', 'Authentication', 'Payments',
+	function($scope, $window, $log, $timeout, Users, Authentication, Payments) {
 
-		var price = 3000;
-		$scope.guests = 1;
+		// Default CC info for test successful transaction
+		$scope.number = '4242 4242 4242 4242';
+		$scope.expiry = '12 / 19';
+		$scope.cvc = '999';
+
+		$scope.price = 3000;
 		$scope.user = Authentication.user;
 		$scope.registration = Authentication.user.registration;
+		$scope.guests = Authentication.user.registration ? Authentication.user.registration.guests : 1;
+		$scope.showGuests = false;
+		$scope.additionalGuests = 1;
 		$scope.error = {};
 
 		$scope.name = $scope.user.displayName;
@@ -22,25 +29,35 @@ angular.module('core').controller('RegisterController', ['$scope', '$window', '$
 
 		// Stripe Response Handler
 		$scope.stripeCallback = function (code, result) {
-			$log.log(code, result);
+
+			// First we get a token from the card details that created our customer
 		    if (result.error) {
 				$scope.error.level = 'alert-warning';
 				$scope.error.message = result.error.message;
 				$timeout(function() { $scope.error = {}; }, 5000);
 		    } else {
+
+		    	// Update the user with address details if provided
+				$scope.user.address.street = $scope.addressLine1;
+				$scope.user.address.city = $scope.addressCity;
+				$scope.user.address.state = $scope.addressState;
+				$scope.user.address.zip = $scope.addressZip;
+
+		    	var paidToDate = $scope.registration ?
+			    	$scope.registration.payments.reduce(function(total, pmt) {
+					  return total + pmt.amount;
+					}) : 0;
 		    	var payment = new Payments({
 		    		user: $scope.user,
-		    		amount: $scope.guests * price,
+		    		amount: $scope.guests * $scope.price - paidToDate,
 		    		token: result.id,
 		    		guests: $scope.guests
 		    	});
 		    	payment.$charge(function(response) {
-					console.log(response);
-					$scope.user.address = $scope.user.address || {};
-					$scope.user.address.street = $scope.addressLine1;
-					$scope.user.address.city = $scope.addressCity;
-					$scope.user.address.state = $scope.addressState;
-					$scope.user.address.zip = $scope.addressZip;
+					Authentication.user = response;
+					$scope.error.message = 'Processed successfully. Thank you for your payment!';
+					$scope.error.level = 'alert-success';
+					$timeout(function() { $scope.error = {}; }, 5000);
 				}, function(errorResponse) {
 					$log.log(errorResponse);
 					$scope.error.message = errorResponse.data.message;
