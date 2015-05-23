26600282c38534fcf1bac9186bb74e19cb0e77a5
Tor Gilbertson <sterlingrobot@gmail.com>
2015-05-23 10:04:19 -0600

    Add menu items, change Articles to Posts

--- public/modules/core/config/core.client.config.js
+++ public/modules/core/config/core.client.config.js
@@ -4,4 +4,11 @@ angular.module('core').config(function() {
 
 
 	}
-);
+)
+.run(['Menus',
+	function(Menus) {
+		// Set top bar menu item
+		Menus.addMenuItem('topbar', 'The\ Event', 'event', 'item');
+		Menus.addMenuItem('topbar', 'The\ SWAG', 'swag', 'item');
+	}
+]);
